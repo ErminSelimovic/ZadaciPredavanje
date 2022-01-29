2a49603b38c0bea62a5b146b6e6ad8aabea4d003
ErminSelimovic <92504422+ErminSelimovic@users.noreply.github.com>
2022-01-29 19:01:23 +0100

    Update ZadatakArray.java

--- Arrays/ZadatakArray.java
+++ Arrays/ZadatakArray.java
@@ -7,9 +7,9 @@ public class Main {
     public static void main(String[] args) {
 
         int[] intArray = new int[]{4,6,7,13,56,21,23,44,19,88};
-        int sum = intArray[0]+intArray[1]+intArray[2]+intArray[3]+intArray[4]+intArray[5]+intArray[6]+intArray[7]+intArray[8]+intArray[9];
+        int sum = intArray[1]+intArray[3]+intArray[5]+intArray[7]+intArray[9];
         System.out.println("Jednodimenzionalni niz: " +Arrays.toString(intArray)+ "\n");
-        System.out.println("Zbir svih brojava u nizu je Suma = " +sum+ "\n");
+        System.out.println("Zbir svake druge vrijednosti niza je = " +sum+ "\n");
 
         int[][] _ddArray = new int[][]{{45,65,48},{884,28,73}};
         System.out.println(Arrays.toString(_ddArray[0]));
@@ -17,6 +17,7 @@ public class Main {
 
         System.out.println();
         System.out.println("Prikaz [1][1] elemnta u nizu je: " +_ddArray[1][1]);
+        System.out.println("\nZbir vrijednosti 0 kolone niza je= " + (_ddArray[0][0]+_ddArray[1][0]));
 
     }
 }
