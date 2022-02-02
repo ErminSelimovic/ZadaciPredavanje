1af612185b5c442878bd7f5f9d138292b7acebc1
ErminSelimovic <92504422+ErminSelimovic@users.noreply.github.com>
2022-02-02 19:05:11 +0100

    Create Main.java

---
+++ Zadatak2/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int firstVariable, secondVariable;
+        firstVariable = 655;
+        secondVariable = 122;
+        System.out.println("First number is " + firstVariable + " and second is " + secondVariable);
+        System.out.print("Sum of this two numbers is ");
+        System.out.println(firstVariable + secondVariable);
+
+        System.out.println("**********************");
+
+
+        double fDoubleNumber, sDoubleNumber;
+        fDoubleNumber = 432.45;
+        sDoubleNumber = 211.39;
+        String textDoubleNumber = "Sum of this (" + fDoubleNumber + " and " + sDoubleNumber + ") two double number is ";
+
+        System.out.print(textDoubleNumber);
+        System.out.println(fDoubleNumber + sDoubleNumber);
+
+
+    }
+}
